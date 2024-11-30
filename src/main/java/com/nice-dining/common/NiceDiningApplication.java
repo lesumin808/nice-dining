9eab139ad789e778600930aee6d990768450c62a
lesumin808 <76189535+lesumin808@users.noreply.github.com>
2024-11-30 10:57:09 +0900

    241130 프로젝트 생성

---
+++ src/main/java/com/nice-dining/common/NiceDiningApplication.java
@@ -0,0 +1,13 @@
+package com.project.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingProjectApplication.class, args);
+	}
+
+}
